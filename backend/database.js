4fcf4a5e35f3b8f6c94950100f752b1e6325fc71
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2018-10-15 23:13:13 -0300

    correcoes inicializacao db

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const USERS = require('./models/users');
 
-module.exports = { connectMongoDB };
+module.exports = { connectMongoDB, createDefaultUsers };
 
 function connectMongoDB() {
   mongoose.connect(
@@ -12,4 +13,22 @@ function connectMongoDB() {
       useNewUrlParser: true
     }
   );
+}
+
+function createDefaultUsers() {
+
+  USERS.findByCode(123, (err, user) => {
+    if (!user || !user.length) {
+      const user1 = new USERS({user: 'user1', password: 123});
+      user1.save();
+    }
+  });
+
+  USERS.findByCode(456, (err, user) => {
+    if (!user || !user.length) {
+      const user2 = new USERS({user: 'user1', password: 456});
+      user2.save();
+    }
+  });
+
 }
--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 auth.setUpJWT();
 database.connectMongoDB();
+database.createDefaultUsers();
 
 app.use('/', statusRoute);
 app.use('/', loginRoute);
